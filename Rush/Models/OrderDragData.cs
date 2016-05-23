cc8d89e827020e869674d766e3d0b52776f31dac
shanaka rusith <shanaka rusith>
2016-05-23 15:29:32 +0530

    before change

---
+++ Rush/Models/OrderDragData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rush.Models
+{
+    public class OrderDragData
+    {
+        public string Name { get; set; }
+    }
+}
