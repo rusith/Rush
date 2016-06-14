4f17b1c1a7ce2ba579928bfe8ae16eecabf73a4a
shanaka rusith <shanaka rusith>
2016-06-14 20:53:31 +0530

    before implement process flow

--- Rush/Models/FileInformation.cs
+++ Rush/Models/FileInformation.cs
@@ -5,21 +5,21 @@ namespace Rush.Models
 {
     public class FileInformation
     {
+        protected bool Equals(FileInformation other)
+        {
+            return IsDuplicate.Equals(other.IsDuplicate) && Equals(Duplicate, other.Duplicate) && Equals(SourceFile, other.SourceFile) && Equals(DestinationFile, other.DestinationFile);
+        }
+
         public override int GetHashCode()
         {
             unchecked
             {
-                var hashCode = SourceFile?.GetHashCode() ?? 0;
-                hashCode = (hashCode*397) ^ (DestinationFile?.GetHashCode() ?? 0);
-                hashCode = (hashCode*397) ^ IsDuplicate.GetHashCode();
-                hashCode = (hashCode*397) ^ (Duplicate?.GetHashCode() ?? 0);
+                var hashCode = IsDuplicate.GetHashCode();
+                hashCode = (hashCode*397) ^ (Duplicate != null ? Duplicate.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (SourceFile != null ? SourceFile.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (DestinationFile != null ? DestinationFile.GetHashCode() : 0);
                 return hashCode;
             }
-        }
-
-        protected bool Equals(FileInformation other)
-        {
-            return Equals(SourceFile, other.SourceFile) && Equals(DestinationFile, other.DestinationFile) && IsDuplicate.Equals(other.IsDuplicate) && Equals(Duplicate, other.Duplicate);
         }
 
         public bool IsDuplicate { get; set; }
@@ -29,11 +29,5 @@ namespace Rush.Models
         public FileInfo SourceFile { get; set; }
         public FileInfo DestinationFile { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            return obj.GetType() == GetType() && Equals((FileInformation) obj);
-        }
     }
 }
